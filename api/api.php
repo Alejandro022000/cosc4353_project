f83014536383a72bb2029f39d649cf28691bed69
Alejandro022000 <ale07pg@gmail.com>
2024-02-12 10:43:39 -0600

    test

--- api/api.php
+++ api/api.php
@@ -17,13 +17,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
 // Function to connect to the database
 function connectToDatabase() {
-// Replace these with actual values for testing
-$serverName = "tcp:myserverale.database.windows.net,1433";
-$database = "mydatabase";
-$username = "aleadmin";
-$password = "456456asdAa!";
-
-
+    $serverName = "tcp:myserverale.database.windows.net,1433";
+    $database = "mydatabase";
+    $username = "aleadmin";
+    $password = "456456asdAa!"; 
+    
     try {
         $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
